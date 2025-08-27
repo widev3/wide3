11e7bbe8bad8b1ccffb28e07203dd048bbde4ed3
gttrcr <gttrcr@gmail.com>
2025-08-27 18:02:21 +0200

    fix nproc in make -j

--- pipeline/sdr++.sh
+++ pipeline/sdr++.sh
@@ -16,7 +16,8 @@ make -j $(nproc)
 sudo make install
 sudo ldconfig
 cd ~
-rm -rf codec2-1.2.0 1.2.0.zip
+rm -rf codec2-1.2.0
+rm -rf 1.2.0.zip
 
 cd ~
 wget https://www.sdrplay.com/software/SDRplay_RPi_Scripts_v0.3.zip
@@ -26,26 +27,18 @@ sed -i 's/ARM32-3.07.2.run/ARM64-3.07.1.run/g' ./SDRplayAPI/1installAPI.sh
 sh ./SDRplayAPI/1installAPI.sh
 sudo systemctl start sdrplay
 cd ~
-rm -rf SDRplayAPI  SDRplay_RPi_Scripts_v0.3.zip  SDRplay_RSP_API-ARM64-3.07.1.run
+rm -rf SDRplayAPI
+rm -rf SDRplay_RPi_Scripts_v0.3.zip
+rm -rf SDRplay_RSP_API-ARM64-3.07.1.run
 
-cd ~
 sudo apt update -y
+sudo apt upgrade -y
+sudo apt autoremove -y
 sudo apt install libglfw3 -y
 sudo apt install libglfw3-dev -y
 sudo apt install libfftw3-3 -y
 sudo apt install libfftw3-dev -y
 sudo apt install libvolk2-dev -y
-
-
-# git clone https://github.com/AlexandreRouma/SDRPlusPlus
-wget https://github.com/AlexandreRouma/SDRPlusPlus/archive/refs/tags/nightly.zip
-unzip nightly.zip
-rm -rf nightly.zip
-cd SDRPlusPlus-nightly
-mkdir build
-cd build
-
-
 sudo apt install libzstd-dev -y
 sudo apt install libairspy-dev -y
 sudo apt install libairspyhf-dev -y
@@ -54,6 +47,14 @@ sudo apt install libhackrf-dev -y
 sudo apt install libiio-dev -y
 sudo apt install libad9361-dev -y
 sudo apt install librtlsdr-dev -y
+
+cd ~
+git clone https://github.com/AlexandreRouma/SDRPlusPlus
+# wget https://github.com/AlexandreRouma/SDRPlusPlus/archive/refs/tags/nightly.zip
+# unzip nightly.zip
+cd SDRPlusPlus
+mkdir build
+cd build
 
 rm -rf *
 cmake .. \
@@ -64,7 +65,7 @@ cmake .. \
   -DOPT_BUILD_FILE_SOURCE=OFF \
   -DOPT_BUILD_HACKRF_SOURCE=OFF \
   -DOPT_BUILD_HERMES_SOURCE=OFF \
-  -DOPT_BUILD_LIMESDR_SOURCE=ON \
+  -DOPT_BUILD_LIMESDR_SOURCE=OFF \
   -DOPT_BUILD_PERSEUS_SOURCE=OFF \
   -DOPT_BUILD_PLUTOSDR_SOURCE=ON \
   -DOPT_BUILD_RFSPACE_SOURCE=OFF \
@@ -96,7 +97,7 @@ cmake .. \
   -DOPT_BUILD_SCANNER=OFF \
   -DOPT_BUILD_SCHEDULER=OFF
 
-make -j $(nproc)
+make -j 1
 sudo make install
 sudo ldconfig
 cd ~
@@ -104,9 +105,10 @@ rm -rf ~/.config/sdrpp/ #in case of previous installations
 rm -rf SDRPlusPlus-nightly
 
 cd ~
-wget https://github.com/gttrcr/whistle_of_wind/archive/refs/tags/25.08.27.zip
-unzip 25.08.27.zip
-cd whistle_of_wind-25.08.27/mount/server
+git clone https://github.com/gttrcr/whistle_of_wind
+# wget https://github.com/gttrcr/whistle_of_wind/archive/refs/tags/25.08.27.zip
+# unzip 25.08.27.zip
+cd whistle_of_wind/mount/server #whistle_of_wind-25.08.27/mount/server
 sudo apt install python3-pip -y
 python3 -m venv u
 source u/bin/activate
