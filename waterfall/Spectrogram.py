fix dataframe rotation

waterfall/Spectrogram.py
@@ -1,9 +1,9 @@
-import pandas as pd
 import io
-import numpy as np
-import time
 import datetime
-import re
+import numpy as np
+import pandas as pd
+from io import StringIO
+from dateutil import parser
 
 
 class Spectrogram(object):
@@ -36,101 +36,48 @@ class Spectrogram(object):
         return pd.read_csv(io.StringIO("".join(file)), sep=self.__separator)
 
     def __spectrogram(self, file):
-        def qty_and_unit(string):
-            match = re.match(r"(.+)\s\[(.+)\]", string)
-            if match:
-                quantity = match.group(1).strip()  # Group 1 is the quantity
-                unit = match.group(2).strip()  # Group 2 is the unit of measure
-                return quantity, unit
-            return None, None
-
-        def milliseconds(strptime):
-            return int(
-                (
-                    strptime.microsecond
-                    + 1000000
-                    * (strptime.second + strptime.minute * 60 + strptime.hour * 3600)
-                )
-                / 1000
-            )
-
-        if len(file) < 3:
-            return None
-
-        # absolute timestamp
-        absolute_tss = []
-        try:
-            absolute_tss = list(
-                map(
-                    lambda x: time.mktime(
-                        datetime.datetime.strptime(x, "%H:%M:%S %d/%m/%Y").timetuple()
-                    ),
-                    pd.read_csv(
-                        io.StringIO(file[0]), sep=self.__separator, header=None
-                    ).values[0][1:-1],
-                )
-            )
-        except:
+        def try_parse_dt(d):
             try:
-                absolute_tss = list(
-                    map(
-                        lambda x: time.mktime(
-                            datetime.datetime.strptime(
-                                x, "%H:%M:%S %m/%d/%Y"
-                            ).timetuple()
-                        ),
-                        pd.read_csv(
-                            io.StringIO(file[0]), sep=self.__separator, header=None
-                        ).values[0][1:-1],
-                    )
-                )
+                return parser.parse(d, dayfirst=True).timestamp()
             except:
-                return None
+                try:
+                    return parser.parse(d, dayfirst=False).timestamp()
+                except:
+                    try:
+                        return datetime.datetime.strptime(d, "%H:%M:%S:%f").timestamp()
+                    except:
+                        return None
 
-        # relative time interval with respect to start)
-        try:
-            relative_tss_zero_end = list(
-                map(
-                    lambda x: milliseconds(
-                        datetime.datetime.strptime(x, "%H:%M:%S:%f")
-                    ),
-                    pd.read_csv(
-                        io.StringIO(file[1]), sep=self.__separator, header=None
-                    ).values[0][1:-1],
-                )
-            )
-        except:
-            return None
+        csv_data = "\n".join(file)
+        df = pd.read_csv(StringIO(csv_data), header=[0, 1])
+        df = df.dropna(axis=1, subset=[df.index[-1]], how="any")
+        magnitude = df.copy()
+        magnitude.columns = range(magnitude.shape[1])
+        magnitude = magnitude.drop(columns=0)
+        magnitude.columns = range(magnitude.shape[1])
+        magnitude = magnitude.drop(index=0)
+        magnitude.index -= 1
+        magnitude = magnitude.astype(float).values.tolist()
 
-        relative_tss_zero_start = list(
-            map(lambda x: relative_tss_zero_end[0] - x, relative_tss_zero_end)
-        )
+        abs_ts, rel_ts = zip(*df.columns)
+        abs_ts = abs_ts[1:]
+        abs_tss = list(map(lambda x: try_parse_dt(x), abs_ts))
+        rel_ts = rel_ts[1:]
+        zero_time = try_parse_dt(rel_ts[0])
+        rel_tss = list(map(lambda x: try_parse_dt(x), rel_ts))
+        rel_tss = list(map(lambda x: zero_time - x if x else None, rel_tss))
 
-        spec = pd.read_csv(io.StringIO("\n".join(file[2:])), sep=self.__separator)
-        columns = spec.columns
-        for column in columns:
-            spec = spec.drop(column, axis=1) if np.isnan(spec[column]).all() else spec
-        spec = spec.rename(
-            columns=dict(
-                map(
-                    lambda x: (spec.columns[x[0] + 1], x[1]),
-                    enumerate(relative_tss_zero_start),
-                )
-            )
-        )
+        frequency = list(map(lambda x: float(x), df.iloc[:, 0][1:]))
 
-        frequency = spec[spec.columns[0]]  # frequencies
-        magnitude = np.array(list(map(lambda x: x[1:], spec.values)))  # magnitudes
+        # TODO improve by computing programmatically the um
         um = {}
         um["time"] = "ms"
-        um["frequency"] = qty_and_unit(frequency.name)[1]
-        um["magnitude"] = tuple(
-            list(set(map(lambda x: qty_and_unit(x), columns[1:-1])))[0]
-        )
+        um["frequency"] = "Hz"
+        um["magnitude"] = "dBm"
 
         return {
-            "r": relative_tss_zero_start,
-            "a": absolute_tss,
+            "r": rel_tss,
+            "a": abs_tss,
             "f": list(map(lambda x: x + self.__lo, frequency)),
             "m": magnitude,
             "u": um,
@@ -200,4 +147,4 @@ class Spectrogram(object):
         return self.spec["m"][x]
 
     def freq_slice(self, x):
-        return self.spec["m"][:, x]
+        return [item[x] for item in self.spec["m"]]

waterfall/ux/MplSpecCanvas.py
@@ -14,8 +14,8 @@ class MplSpecCanvas(FigureCanvasQTAgg):
 
         self.fig = Figure()
         self.axes = self.fig.add_subplot(111)
-        self.axes.set_xlabel("frequency")
-        self.axes.set_ylabel("time")
+        self.axes.set_xlabel("time")
+        self.axes.set_ylabel("frequency")
 
         self.__xlim = self.axes.get_xlim()
         self.__ylim = self.axes.get_ylim()
@@ -45,12 +45,26 @@ class MplSpecCanvas(FigureCanvasQTAgg):
             interpolation="none",
             origin="lower",
             extent=[
-                min(self.__sp["f"]),
-                max(self.__sp["f"]),
                 min(self.__sp["r"]),
                 max(self.__sp["r"]),
+                min(self.__sp["f"]),
+                max(self.__sp["f"]),
             ],
         )
+
+        mx = min(self.__sp["r"])
+        Mx = max(self.__sp["r"])
+        self.axes.set_xticks(np.arange(mx, Mx, (Mx - mx) / 10))
+        self.axes.set_xticks(np.arange(mx, Mx, (Mx - mx) / 30), minor=True)
+
+        my = min(self.__sp["f"])
+        My = max(self.__sp["f"])
+        self.axes.set_yticks(np.arange(my, My, (My - my) / 10))
+        self.axes.set_yticks(np.arange(my, My, (My - my) / 30), minor=True)
+
+        self.axes.grid(which="minor", alpha=0.2)
+        self.axes.grid(which="major", alpha=0.5)
+
         self.fig.tight_layout()
         self.draw()
 